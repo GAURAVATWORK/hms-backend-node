dc03339488a997b484a28d3442b1414b86315cc9
Gaurav Singh <gauravatwork23@gmail.com>
2026-09-11 23:53:23 +0530

    feat: seed doctor qualifications

---
+++ src/database/migrations/016_seed_qualifications.sql
@@ -0,0 +1,112 @@
+INSERT INTO qualifications (
+    code,
+    name,
+    description,
+    is_active,
+    sort_order
+)
+VALUES
+
+(
+    'MBBS',
+    'MBBS',
+    'Bachelor of Medicine, Bachelor of Surgery',
+    TRUE,
+    1
+),
+
+(
+    'MD',
+    'MD',
+    'Doctor of Medicine',
+    TRUE,
+    2
+),
+
+(
+    'MS',
+    'MS',
+    'Master of Surgery',
+    TRUE,
+    3
+),
+
+(
+    'DNB',
+    'DNB',
+    'Diplomate of National Board',
+    TRUE,
+    4
+),
+
+(
+    'PG_DIPLOMA',
+    'PG Diploma',
+    'Postgraduate Medical Diploma',
+    TRUE,
+    5
+),
+
+(
+    'DM',
+    'DM',
+    'Doctorate of Medicine',
+    TRUE,
+    6
+),
+
+(
+    'MCH',
+    'MCh',
+    'Master of Chirurgiae',
+    TRUE,
+    7
+),
+
+(
+    'BDS',
+    'BDS',
+    'Bachelor of Dental Surgery',
+    TRUE,
+    8
+),
+
+(
+    'MDS',
+    'MDS',
+    'Master of Dental Surgery',
+    TRUE,
+    9
+),
+
+(
+    'BPT',
+    'BPT',
+    'Bachelor of Physiotherapy',
+    TRUE,
+    10
+),
+
+(
+    'MPT',
+    'MPT',
+    'Master of Physiotherapy',
+    TRUE,
+    11
+),
+
+(
+    'BSC_NUTRITION_DIETETICS',
+    'BSc Nutrition and Dietetics',
+    'Bachelor of Science in Nutrition and Dietetics',
+    TRUE,
+    12
+),
+
+(
+    'MSC_NUTRITION_DIETETICS',
+    'MSc Nutrition and Dietetics',
+    'Master of Science in Nutrition and Dietetics',
+    TRUE,
+    13
+);
